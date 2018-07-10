49fedb0b5aeeea30ad0453f847dccf853fc4fab6
Code It Like Miley <codeitlikemiley@gmail.com>
2018-07-10 10:20:11 +0800

    updated Settings

--- resources/assets/js/pages/Settings.vue
+++ resources/assets/js/pages/Settings.vue
@@ -5,51 +5,59 @@
       icons 
       centered
     >
-      <v-toolbar color="white">
-        <v-tabs-bar>
-          <v-tabs-slider color="primary"/>
-          <!-- tabs -->
-          <v-tabs-item 
-            href="#account" 
-            class="accent--text"
-          >
-            <v-icon color="indigo" large>fa-user</v-icon>
-            <span 
-              :class="$vuetify.breakpoint.width >= 600 && 'title'"
-            >
-              Account
-            </span>
-          </v-tabs-item>
-          <v-tabs-item 
-            href="#profile" 
-            class="accent--text"
-          >
-            <v-icon color="teal" large>fa-address-card</v-icon>
-            <span 
-              :class="$vuetify.breakpoint.width >= 600 && 'title'"
-            >
-              Profile
-            </span>
-          </v-tabs-item>
-          <v-tabs-item 
-            href="#referral-link" 
-            class="accent--text"
-          >
-            <v-icon color="cyan" large>link</v-icon>
-            <span 
-              :class="$vuetify.breakpoint.width >= 600 && 'title'"
-            >
-              Referral Link
-            </span>
-          </v-tabs-item>
-          <!-- tabs -->
-        </v-tabs-bar>
-      </v-toolbar>
+      <v-tabs-slider color="primary"/>
+      <!-- tabs -->
+      <v-tab 
+        href="#account" 
+        class="accent--text"
+      >
+        <v-icon 
+          color="indigo" 
+          large>fa-user</v-icon>
+        <span 
+          :class="$vuetify.breakpoint.width >= 600 && 'title'"
+        >
+          Account
+        </span>
+      </v-tab>
+      <v-tab 
+        href="#profile" 
+        class="accent--text"
+      >
+        <v-icon 
+          color="teal" 
+          large>fa-address-card</v-icon>
+        <span 
+          :class="$vuetify.breakpoint.width >= 600 && 'title'"
+        >
+          Profile
+        </span>
+      </v-tab>
+      <v-tab 
+        href="#referral-link" 
+        class="accent--text"
+      >
+        <v-icon 
+          color="cyan" 
+          large>link</v-icon>
+        <span 
+          :class="$vuetify.breakpoint.width >= 600 && 'title'"
+        >
+          Referral Link
+        </span>
+      </v-tab>
+      <!-- tabs -->
       <v-tabs-items>
         <!-- tab contents -->
-        <account/>
-        <profile/>
-        <referral-link/>
+        <v-tab-item id="account">
+          <account/>
+        </v-tab-item>
+        <v-tab-item id="profile">
+          <profile/>
+        </v-tab-item>
+        <v-tab-item id="referral-link">
+          <referral-link/>
+        </v-tab-item>
         <!-- tab contents -->
       </v-tabs-items>
     </v-tabs>
@@ -57,20 +65,20 @@
 </template>
 
 <script>
-import MainLayout from 'Layouts/Main.vue'
-import Account from 'Components/settings/Account.vue'
-import Profile from 'Components/settings/Profile.vue'
-import ReferralLink from 'Components/settings/ReferralLink.vue'
+import MainLayout from "Layouts/Main.vue";
+import Account from "Components/settings/Account.vue";
+import Profile from "Components/settings/Profile.vue";
+import ReferralLink from "Components/settings/ReferralLink.vue";
 
 export default {
-    components: {
-        MainLayout,
-        Account,
-        Profile,
-        ReferralLink
-    },
-    data: () => ({
-        contentClass: { 'grey': true, 'lighten-4': true, 'accent--text': true }
-    })
-}
+  components: {
+    MainLayout,
+    Account,
+    Profile,
+    ReferralLink
+  },
+  data: () => ({
+    contentClass: { grey: true, "lighten-4": true, "accent--text": true }
+  })
+};
 </script>
