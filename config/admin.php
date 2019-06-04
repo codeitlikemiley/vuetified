a2eb6348538f009e8307b3369840c317a26a3604
Uriah Galang <codeitlikemiley@gmail.com>
2019-06-04 17:01:16 +0800

    update admin default pass

--- config/admin.php
+++ config/admin.php
@@ -4,5 +4,5 @@ return [
     'username' => env('ADMIN_USERNAME', 'admin'),
     'name'     => env('ADMIN_NAME', 'admin'),
     'email'    => env('ADMIN_EMAIL', 'admin@example.com'),
-    'password' => env('ADMIN_PASSWORD', 'secret')
+    'password' => env('ADMIN_PASSWORD', 'secretpass')
 ];
