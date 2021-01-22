c89d757913505a0ec8eeae41cfd85013c83734d3
Goldcoders <goldcoders@protonmail.com>
2021-01-22 21:57:07 +0800

    added inertia template for blade

---
+++ resources/views/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
+    <script src="{{ mix('/js/app.js') }}" defer></script>
+</head>
+<body>
+@inertia
+</body>
+
+</html>
