368985074026a94c37cf122edfed8aa88e12ee72
Goldcoders <goldcoders@protonmail.com>
2021-01-23 01:20:18 +0800

    added auth route

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::impersonate();
 
 Route::get('/', [HomeController::class, 'home'])->name('home');
+
+Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('login', [LoginController::class, 'login'])->name('login.attempt');
+Route::get('logout', [LoginController::class, 'showLogoutForm'])->name('logout')->middleware('auth');
+Route::post('logout', [LoginController::class, 'logout'])->name('logout.attempt')->middleware('auth');
