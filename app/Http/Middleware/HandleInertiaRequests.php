e96b380dd8fbf2ea8ad5210f819adaee2c46121d
Goldcoders <goldcoders@protonmail.com>
2021-01-23 01:16:20 +0800

    added state

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -37,7 +37,35 @@ class HandleInertiaRequests extends Middleware
     public function share(Request $request)
     {
         return array_merge(parent::share($request), [
-            //
+            'app' => function () use ($request) {
+                return ['name' => config('app.name')];
+            },
+            'auth' => function () use ($request) {
+                $user = $request->user();
+                $impersonating = $request->session()->get('impersonated_by');
+                // $routeName = $request->route()->getName();
+
+                return [
+                    'isLoggedIn' => $user ? true : false,
+                    'isImpersonating' => $impersonating,
+                    'user' => $user ? [
+                        'id' => $user->id,
+                        'email' => $user->email,
+                        'name' => $user->name,
+                    ] : null,
+                ];
+            },
+            'flash' => function () use ($request) {
+                return [
+                    'success' => $request->session()->get('success'),
+                    'warning' => $request->session()->get('warning'),
+                    'info' => $request->session()->get('info'),
+                    'error' => $request->session()->get('error'),
+                ];
+            },
+            'errors' => function () use ($request) {
+                return $request->session()->get('errors') ? $request->session()->get('errors')->getBag('default')->getMessages() : (object) [];
+            },
         ]);
     }
 }
