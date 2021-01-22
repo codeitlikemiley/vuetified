728dca251a573c4aa279f37ee67f7fa1aaa4c03c
Goldcoders <goldcoders@protonmail.com>
2021-01-23 01:16:38 +0800

    added dashboard

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,9 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/';
+    public const DASHBOARD = '/dashboard';
+
 
     /**
      * The controller namespace for the application.
