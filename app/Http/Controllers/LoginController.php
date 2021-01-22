dcd12ec018874cd82ec6ebf94a35f7624979ef7c
Goldcoders <goldcoders@protonmail.com>
2021-01-23 01:16:27 +0800

    add login

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Inertia\Inertia;
+
+class LoginController extends Controller
+{
+    use AuthenticatesUsers;
+
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+     */
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = RouteServiceProvider::DASHBOARD;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except(['logout', 'showLogoutForm']);
+    }
+
+    public function showLoginForm()
+    {
+        return Inertia::render('Login');
+    }
+
+    public function showLogoutForm()
+    {
+        return Inertia::render('Logout');
+    }
+}
