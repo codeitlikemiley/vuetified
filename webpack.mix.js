57cdefc59ca85f12f4d21bffb73f284cbdcdcc7c
Goldcoders <goldcoders@protonmail.com>
2021-01-22 21:58:55 +0800

    update to use vuetify loader

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,17 +1,19 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
+const path = require("path");
+const VuetifyLoaderPlugin = require("vuetify-loader/lib/plugin");
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
- | file for the application as well as bundling up all the JS files.
- |
- */
-
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.js("resources/js/app.js", "public/js")
+    .sass("resources/sass/app.scss", "public/css")
+    .webpackConfig({
+        output: { chunkFilename: "js/[name].js?id=[chunkhash]" },
+        resolve: {
+            alias: {
+                vue$: "vue/dist/vue.runtime.esm.js",
+                "@": path.resolve("resources/js"),
+                ziggy: path.resolve('vendor/tightenco/ziggy/dist'),
+            },
+        },
+        plugins: [new VuetifyLoaderPlugin()],
+    })
+    .version()
+    .sourceMaps();
