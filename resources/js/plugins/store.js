db8e8a5b27f0abd607e60a2d6c44a1a2bf087114
Goldcoders <goldcoders@protonmail.com>
2021-01-23 01:19:18 +0800

    remove comment

--- resources/js/plugins/store.js
+++ resources/js/plugins/store.js
@@ -9,12 +9,7 @@ Vue.use(Vue2Storage, {
     ttl: 0 // disables the lifetime and the record will be kept forever
     // ttl: 60 * 60 * 24 * 1000 // 24 hours
 });
-
-// create a global mixins that checks the value of storage of users, permissions , roles
-// localstorage non expiring
-// if on created method no keys is present or expired
-
-
+  
 // add this event to remove all ls at window closed
 // window.onbeforeunload = function() {
 //     localStorage.removeItem(key);
