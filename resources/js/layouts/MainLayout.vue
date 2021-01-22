ef8d19c68aadfa25d4f1072930358a5ff18b09ce
Goldcoders <goldcoders@protonmail.com>
2021-01-23 01:17:37 +0800

    added layouts

---
+++ resources/js/layouts/MainLayout.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-app>
+    <app-nav-bar :title="title"/>
+    <left-side-bar/>
+    <v-main transition="slide-x-transition" fluid pa-0 ma-0>
+      <slot/>
+    </v-main>
+    <app-footer/>
+  </v-app>
+</template>
+
+<script>
+import AppFooter from "../partials/AppFooter.vue";
+import AppNavBar from "../partials/AppNavBar.vue";
+import LeftSideBar from "../partials/LeftSideBar.vue";
+
+export default {
+  components: {
+    AppFooter,
+    AppNavBar,
+    LeftSideBar,
+  },
+  props: {
+    title: String
+  },
+  mounted() {
+    this.updatePageTitle(this.title);
+  },
+  methods: {
+    updatePageTitle(title) {
+      document.title = title
+        ? `${title} | ${this.$page.app.name}`
+        : this.$page.app.name;
+    }
+  },
+  watch: {
+    title(title) {
+      this.updatePageTitle(title);
+    }
+  }
+};
+</script>
---
+++ resources/js/layouts/ModalLayout.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-app>
+    <div>
+      <slot name="toolbar" />
+    </div>
+
+    <v-main>
+      <slot />
+    </v-main>
+    <v-footer app>
+      <slot name="footer" />
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+  },
+  watch: {
+    title(title) {
+      this.updatePageTitle(title);
+    },
+  },
+  mounted(){
+    this.updatePageTitle(this.title);
+  },
+  methods: {
+    updatePageTitle(title) {
+      document.title = title
+        ? `${title} | ${this.$page.app.name}`
+        : this.$page.app.name;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
