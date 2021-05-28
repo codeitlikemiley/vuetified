b234fbed91d3d0fd2433e121c74d79cbedcd93fd
Goldcoders <goldcoders@protonmail.com>
2021-05-28 17:43:43 +0800

    conditionally use mix for production and local

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,17 +3,32 @@ const path = require("path");
 const VuetifyLoaderPlugin = require("vuetify-loader/lib/plugin");
 
 mix.js("resources/js/app.js", "public/js")
-    .sass("resources/sass/app.scss", "public/css")
-    .webpackConfig({
-        output: { chunkFilename: "js/[name].js?id=[chunkhash]" },
-        resolve: {
-            alias: {
-                vue$: "vue/dist/vue.runtime.esm.js",
-                "@": path.resolve("resources/js"),
-                ziggy: path.resolve('vendor/tightenco/ziggy/dist'),
+    .sass("resources/sass/app.scss", "public/css");
+    
+    if (mix.inProduction()) {
+        mix.webpackConfig({
+            output: { chunkFilename: "js/[name].js?id=[chunkhash]" },
+            resolve: {
+                alias: {
+                    vue$: "vue/dist/vue.runtime.esm.js",
+                    "@": path.resolve("resources/js"),
+                    ziggy: path.resolve('vendor/tightenco/ziggy/dist'),
+                },
             },
-        },
-        plugins: [new VuetifyLoaderPlugin()],
-    })
-    .version()
-    .sourceMaps();
+            plugins: [new VuetifyLoaderPlugin()],
+        })
+        .version()
+        .sourceMaps();
+    }else{
+        mix.webpackConfig({
+            output: { chunkFilename: "js/[name].js?id=[chunkhash]" },
+            resolve: {
+                alias: {
+                    vue$: "vue/dist/vue.runtime.esm.js",
+                    "@": path.resolve("resources/js"),
+                },
+            },
+            plugins: [new VuetifyLoaderPlugin()],
+        })
+        .sourceMaps();
+    }
