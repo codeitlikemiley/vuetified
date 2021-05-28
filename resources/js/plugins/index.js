ddc7a0a47b238930526d06793ed28b7bffc19bee
Goldcoders <goldcoders@protonmail.com>
2021-05-28 17:45:37 +0800

    remote route-local.js

--- resources/js/plugins/index.js
+++ resources/js/plugins/index.js
@@ -1,7 +1,7 @@
-import "./vee-validate";
-// import "./route"; // uncomment for production
-import "./route-local";
+
 import "./bus";
-import "./store";
 import "./session";
+import "./store";
+import "./vee-validate";
 import "./websocket";
+// import "./route"; // uncomment for production
